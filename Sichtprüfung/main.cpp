7aa93c1ade8a58cac145a9659fce6f7a42d4de82
Daniel Kosinski <daniel.kosinski1991@gmail.com>
2016-11-11 18:08:51 +0100

    Comments added

--- Sichtprüfung/main.cpp
+++ Sichtprüfung/main.cpp
@@ -3,8 +3,11 @@
 
 int main(int argc, char *argv[])
 {
+	//main application
 	QApplication app(argc, argv);
+	//GUI generated from ui file
 	MainGui mainGui;
 	mainGui.show();
+
 	return app.exec();
 }

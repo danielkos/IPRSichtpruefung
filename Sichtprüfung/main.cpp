fe2e3a44797a6ca527e334a6555f252f54df86c6
Daniel Kosinski <daniel.kosinski1991@gmail.com>
2016-11-19 02:32:49 +0100

    Test cases added

--- Sichtprüfung/main.cpp
+++ Sichtprüfung/main.cpp
@@ -1,6 +1,7 @@
 #include "MainGui.h"
 #include <QtWidgets/QApplication>
 #include <opencv2\opencv.hpp>
+#include "HoleVerification.h"
 
 int main(int argc, char *argv[])
 {
@@ -8,11 +9,15 @@ int main(int argc, char *argv[])
 	QApplication app(argc, argv);
 	//GUI generated from ui file
 	MainGui mainGui;
+	VerificationMethod* item = new HoleVerification();
 	cv::Mat src;
 	
 	src = cv::imread(argv[1], true);
 	
 	mainGui.setInputImage(&src);
+	mainGui.addVerificationMethod("1adfadf", item);
+	mainGui.addVerificationMethod("2adfadf", item);
+
 	mainGui.show();
 
 

6658c300fab7884c8e53e7d5bacdd82ea6c6ca72
Daniel Kosinski <daniel.kosinski1991@gmail.com>
2016-11-26 03:15:54 +0100

    Method display style changed

--- Sichtprüfung/GeneratedFiles/ui_Method.h
+++ Sichtprüfung/GeneratedFiles/ui_Method.h
@@ -13,9 +13,8 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
-#include <QtWidgets/QCheckBox>
 #include <QtWidgets/QComboBox>
-#include <QtWidgets/QFrame>
+#include <QtWidgets/QGroupBox>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLineEdit>
@@ -28,10 +27,8 @@ class Ui_Method
 {
 public:
     QHBoxLayout *horizontalLayout_2;
-    QFrame *frameMethod;
-    QHBoxLayout *horizontalLayout;
-    QCheckBox *checkBoxMethod;
-    QFrame *frame;
+    QGroupBox *groupBoxMethod;
+    QHBoxLayout *horizontalLayout_4;
     QRadioButton *radioButtonBoolInput;
     QComboBox *comboBoxParams;
     QLineEdit *lineEditNumInput;
@@ -40,7 +37,7 @@ public:
     {
         if (Method->objectName().isEmpty())
             Method->setObjectName(QStringLiteral("Method"));
-        Method->resize(392, 22);
+        Method->resize(318, 53);
         QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
         sizePolicy.setHorizontalStretch(0);
         sizePolicy.setVerticalStretch(0);
@@ -50,47 +47,34 @@ public:
         horizontalLayout_2->setSpacing(0);
         horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
         horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
-        frameMethod = new QFrame(Method);
-        frameMethod->setObjectName(QStringLiteral("frameMethod"));
-        QSizePolicy sizePolicy1(QSizePolicy::Fixed, QSizePolicy::Fixed);
-        sizePolicy1.setHorizontalStretch(0);
-        sizePolicy1.setVerticalStretch(0);
-        sizePolicy1.setHeightForWidth(frameMethod->sizePolicy().hasHeightForWidth());
-        frameMethod->setSizePolicy(sizePolicy1);
-        horizontalLayout = new QHBoxLayout(frameMethod);
-        horizontalLayout->setSpacing(6);
-        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
-        horizontalLayout->setContentsMargins(1, 1, 1, 1);
-        checkBoxMethod = new QCheckBox(frameMethod);
-        checkBoxMethod->setObjectName(QStringLiteral("checkBoxMethod"));
-
-        horizontalLayout->addWidget(checkBoxMethod);
-
-        frame = new QFrame(frameMethod);
-        frame->setObjectName(QStringLiteral("frame"));
-        frame->setFrameShape(QFrame::VLine);
-        frame->setFrameShadow(QFrame::Raised);
-
-        horizontalLayout->addWidget(frame);
-
-        radioButtonBoolInput = new QRadioButton(frameMethod);
+        groupBoxMethod = new QGroupBox(Method);
+        groupBoxMethod->setObjectName(QStringLiteral("groupBoxMethod"));
+        groupBoxMethod->setAlignment(Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter);
+        groupBoxMethod->setFlat(false);
+        groupBoxMethod->setCheckable(true);
+        groupBoxMethod->setChecked(false);
+        horizontalLayout_4 = new QHBoxLayout(groupBoxMethod);
+        horizontalLayout_4->setSpacing(6);
+        horizontalLayout_4->setObjectName(QStringLiteral("horizontalLayout_4"));
+        horizontalLayout_4->setContentsMargins(-1, 15, -1, 9);
+        radioButtonBoolInput = new QRadioButton(groupBoxMethod);
         radioButtonBoolInput->setObjectName(QStringLiteral("radioButtonBoolInput"));
 
-        horizontalLayout->addWidget(radioButtonBoolInput);
+        horizontalLayout_4->addWidget(radioButtonBoolInput);
 
-        comboBoxParams = new QComboBox(frameMethod);
+        comboBoxParams = new QComboBox(groupBoxMethod);
         comboBoxParams->setObjectName(QStringLiteral("comboBoxParams"));
         comboBoxParams->setMinimumSize(QSize(140, 0));
 
-        horizontalLayout->addWidget(comboBoxParams);
+        horizontalLayout_4->addWidget(comboBoxParams);
 
-        lineEditNumInput = new QLineEdit(frameMethod);
+        lineEditNumInput = new QLineEdit(groupBoxMethod);
         lineEditNumInput->setObjectName(QStringLiteral("lineEditNumInput"));
 
-        horizontalLayout->addWidget(lineEditNumInput);
+        horizontalLayout_4->addWidget(lineEditNumInput);
 
 
-        horizontalLayout_2->addWidget(frameMethod);
+        horizontalLayout_2->addWidget(groupBoxMethod);
 
 
         retranslateUi(Method);
@@ -101,7 +85,7 @@ public:
     void retranslateUi(QWidget *Method)
     {
         Method->setWindowTitle(QApplication::translate("Method", "Form", 0));
-        checkBoxMethod->setText(QApplication::translate("Method", "CheckBox", 0));
+        groupBoxMethod->setTitle(QApplication::translate("Method", "GroupBox", 0));
         radioButtonBoolInput->setText(QString());
     } // retranslateUi
 

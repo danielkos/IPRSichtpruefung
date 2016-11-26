7457a6c4c156fbe30fcbab4fc979e5ad5fff678b
Daniel Kosinski <daniel.kosinski1991@gmail.com>
2016-11-26 01:16:27 +0100

    Implementation changed due to  42d1373b1d566527a85d206334b01fcca0ecba11
    MethodGuiItem handles one checkbox with the method name and a dropdown
    menu, which contains the paramer names of the method. An additional
    linedit or radio button is available dependent on the parameter type.
    Numerical types result in lineedit, boolean in radio button.

--- Sichtprüfung/MethodGuiItem.cpp
+++ Sichtprüfung/MethodGuiItem.cpp
@@ -1,11 +1,29 @@
 #include "MethodGuiItem.h"
 #include <QString>
+#include <QLineEdit>
+#include <QComboBox>
 
-MethodGuiItem::MethodGuiItem(std::string name, QWidget* parent)
-	: QCheckBox(parent)
+MethodGuiItem::MethodGuiItem(std::string name, std::vector<Parameter>& parameters, QWidget* parent)
+	: QWidget(parent), name_(name), parameters_(parameters)
 {
-	this->setText(QString::fromStdString(name));
-	name_ = name;
+	ui_.setupUi(this);
+	ui_.checkBoxMethod->setText(QString::fromStdString(name));
+	
+	//Add paramters to dropdown
+	for (size_t i = 0; i < parameters_.size(); i++)
+	{
+		ui_.comboBoxParams->addItem(parameters_[i].name_, parameters_[i].value_);
+	}
+
+	//Set first parameter
+	ui_.comboBoxParams->setCurrentIndex(0);
+	changeParams(0);
+
+	//Connections to manage inputs and selection of parameters
+	QObject::connect(ui_.comboBoxParams, SIGNAL(currentIndexChanged(int)), this, SLOT(paramIndexChanged(int)));
+	QObject::connect(ui_.lineEditNumInput, SIGNAL(textEdited(QString)), this, SLOT(paramInputChanged(QString)));
+	QObject::connect(ui_.radioButtonBoolInput, SIGNAL(toggled(bool)), this, SLOT(paramInputChanged(bool)));
+	QObject::connect(ui_.checkBoxMethod, &QCheckBox::toggled, this, &MethodGuiItem::changeToNormal);
 }
 
 MethodGuiItem::~MethodGuiItem()
@@ -13,12 +31,83 @@ MethodGuiItem::~MethodGuiItem()
 	
 }
 
+void MethodGuiItem::paramIndexChanged(int index)
+{
+	changeParams(index);
+}
+
+void MethodGuiItem::paramInputChanged(const QString& input)
+{
+	int index = ui_.comboBoxParams->currentIndex();
+	parameters_[index].value_ = input;
+}
+
+void MethodGuiItem::paramInputChanged(bool input)
+{
+	int index = ui_.comboBoxParams->currentIndex();
+	parameters_[index].value_ = input;
+}
+
 bool MethodGuiItem::selected()
 {
-	return isChecked();
+	return ui_.checkBoxMethod->isChecked();
 }
 
 std::string MethodGuiItem::name()
 {
 	return name_;
+}
+
+std::vector<Parameter>& MethodGuiItem::parameters()
+{
+	return parameters_;
+}
+
+void MethodGuiItem::setMode(bool successful)
+{
+	if (successful)
+	{
+		ui_.frameMethod->setFrameShape(QFrame::Shape::Box);
+		ui_.frameMethod->setStyleSheet("#frameMethod{ border: 2px solid green; }");
+	}
+	else
+	{
+		ui_.frameMethod->setFrameShape(QFrame::Shape::Box);
+		ui_.frameMethod->setStyleSheet("#frameMethod{ border: 5px solid red; }");
+	}
+}
+
+void MethodGuiItem::changeParams(int index)
+{
+	switch (parameters_[index].type_)
+	{
+	case QMetaType::Double:
+		ui_.radioButtonBoolInput->setVisible(false);
+		ui_.lineEditNumInput->setVisible(true);
+		ui_.lineEditNumInput->setText(parameters_[index].value_.toString());
+		break;
+	case QMetaType::Int:
+		ui_.radioButtonBoolInput->setVisible(false);
+		ui_.lineEditNumInput->setVisible(true);
+		ui_.lineEditNumInput->setText(parameters_[index].value_.toString());
+		break;
+	case QMetaType::Bool:
+		ui_.lineEditNumInput->setVisible(false);
+		ui_.radioButtonBoolInput->setVisible(true);
+		ui_.radioButtonBoolInput->setChecked(parameters_[index].value_.toBool());
+		break;
+	default:
+		ui_.lineEditNumInput->setVisible(false);
+		ui_.radioButtonBoolInput->setVisible(false);
+		break;
+	}
+}
+
+void MethodGuiItem::changeToNormal(bool toggled)
+{
+	if (!toggled)
+	{
+		ui_.frameMethod->setFrameShape(QFrame::Shape::NoFrame);
+		ui_.frameMethod->setStyleSheet("");
+	}
 }
--- Sichtprüfung/MethodGuiItem.h
+++ Sichtprüfung/MethodGuiItem.h
@@ -3,16 +3,21 @@
 
 #include <QWidget>
 #include <QCheckBox>
+#include <QLabel>
+#include <QRadioButton>
+#include "Parameter.h"
+#include "ui_Method.h"
 
 /**
 * \brief Class which represents a gui item for a verification method
-* Contains a checkbox with a name
+* Contains a checkbox with a name, a dropbox for parameter name 
+* and a line edit/radio button for input
 */
-class MethodGuiItem : public QCheckBox
+class MethodGuiItem : public QWidget
 {
 	Q_OBJECT
 public:
-	MethodGuiItem(std::string name, QWidget* parent = 0);
+	MethodGuiItem(std::string name, std::vector<Parameter>& parameters, QWidget* parent = 0);
 	~MethodGuiItem();
 
 	/**
@@ -25,8 +30,52 @@ public:
 	*/
 	std::string name();
 
+	/**
+	* \brief Returns the parameters from gui
+	* Each parameter change will be stored
+	*/
+	std::vector<Parameter>& parameters();
+
+	/**
+	* \brief Sets the mode of the gui item
+	* If the associated method fails a red box becomes visible
+	* else a green one
+	*/
+	void setMode(bool successful);
+
+public slots:
+	/**
+	* \brief Updates the parameters which will be displayed
+	* \param index index from dropdown
+	*/
+	void paramIndexChanged(int index);
+
+	/**
+	* \brief Updates the current parameter if the type is string, double, int
+	*/
+	void paramInputChanged(const QString& input);
+
+	/**
+	* \brief Updates the current parameter if the type is bool
+	*/
+	void paramInputChanged(bool input);
+
 private:
+	Ui::Method ui_;
 	std::string name_;
+
+	std::vector<Parameter> parameters_;
+
+	/**
+	* \brief Updates the displayed parameters
+	*/
+	void changeParams(int index);
+
+	/**
+	* \Resets the current to normal
+	* Normal means without box
+	*/
+	void changeToNormal(bool toggled);
 };
 
 #endif //MethodGuiItem

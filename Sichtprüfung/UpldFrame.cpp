48c4ac6a6175f4955ec1af08a0a17e83efd03962
Sven Kiesser <sven.kiesser@artiminds.com>
2016-12-09 18:21:58 +0100

    Added more camera parameters

--- Sichtprüfung/UpldFrame.cpp
+++ Sichtprüfung/UpldFrame.cpp
@@ -44,6 +44,18 @@ void UpldFrame::fromCamera()
 
 	cv::putText(frame, "Camera not opened!", cv::Point(50, 50), cv::FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(255));
 
+	// Set camera parameters. Various parameters can be set, see:
+	// http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html
+	if  (videoCapture_ != 0)
+	{ 
+		videoCapture_->set(CV_CAP_PROP_FRAME_WIDTH, CAMERA_WIDHT);
+		videoCapture_->set(CV_CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT);
+		//videoCapture_->set(CV_CAP_PROP_BRIGHTNESS, 2);
+		//videoCapture_->set(CV_CAP_PROP_CONTRAST, 2);
+		videoCapture_->set(CV_CAP_PROP_FPS, CAMERA_FPS);
+
+	}
+
 	while (!exit_)
 	{
 		if (videoCapture_->isOpened())
@@ -51,9 +63,6 @@ void UpldFrame::fromCamera()
 			frame.release();
 			// get a new frame from camera
 			videoCapture_->read(frame);
-
-			// TODO: Kamerauflösung und Einstellungen checken: es kommt immer ein Bild von 640x480 an
-			LOGGER->Log("Camera resolution: %d %d", frame.cols, frame.rows);
 		}
 
 		Q_EMIT newCameraImage(&frame);
--- Sichtprüfung/UpldFrame.h
+++ Sichtprüfung/UpldFrame.h
@@ -29,7 +29,13 @@ Q_SIGNALS:
 private:
 
 	// Time delay (in milliseconds) between the recording of two camera frames.
+	// IPR camera works with 11.2fps => ~89ms per frame
 	const long CAMERA_RECORD_DELAY = 90;
+
+	// Camera parameters
+	const double CAMERA_WIDHT = 2048;
+	const double CAMERA_HEIGHT = 1536;
+	const double CAMERA_FPS = 11.2;
 
 	cv::VideoCapture* videoCapture_;
 	bool exit_;

078fd5d8212788c9f6c3ea7bea6c062e7eb00d2e
Daniel Kosinski <daniel.kosinski1991@gmail.com>
2016-12-12 18:07:51 +0100

    Singelton class to read xml files added

---
+++ Sichtprüfung/ConfigurationStorage.cpp
@@ -0,0 +1,85 @@
+#include "ConfigurationStorage.h"
+
+ConfigurationStorage::ConfigurationStorage()
+{
+
+}
+
+ConfigurationStorage& ConfigurationStorage::instance()
+{
+	static ConfigurationStorage storage;
+	return storage;
+}
+bool ConfigurationStorage::write(const std::string& configPath, std::string node, QVariant value)
+{
+	bool ret = true;
+	if (!storage_.isOpened())
+	{
+		ret = storage_.open(configPath, cv::FileStorage::WRITE);
+	}
+
+	if (value.isNull() && !node.empty())
+	{
+		storage_ << node.c_str();
+	}
+	else if (!value.isNull() && !node.empty())
+	{
+		switch (value.type())
+		{
+		case QMetaType::Int:
+			storage_ << node << value.toInt();
+			break;
+		case QMetaType::Double:
+			storage_ << node << value.toDouble();
+			break;
+		case QMetaType::Bool:
+			storage_ << node << value.toInt();
+			break;
+		case QVariant::String:
+			storage_ << node << value.toString().toStdString();
+			break;
+		}
+	}
+	else
+	{
+		ret = false;
+	}
+	return ret;
+}
+
+bool ConfigurationStorage::read(const std::string& configPath, const std::string& node, int type, QVariant& value)
+{
+	bool ret = true;
+	if (storage_.open(configPath, cv::FileStorage::READ))
+	{
+		if (!node.empty())
+		{
+			switch (type)
+			{
+			case QMetaType::Int:
+				value = QVariant((int)storage_[node]);
+				break;
+			case QMetaType::Double:
+				value = QVariant((double)storage_[node]);
+				break;
+			case QMetaType::Bool:
+				value = QVariant((int)storage_[node]);
+				break;
+			case QMetaType::Char:
+				value = QVariant(QString::fromStdString((std::string)storage_[node]));
+				break;
+			}
+		}
+		storage_.release();
+	}
+	else
+	{
+		ret = false;
+	}
+	return ret;
+}
+
+void ConfigurationStorage::realease()
+{
+	storage_.release();
+}
---
+++ Sichtprüfung/ConfigurationStorage.h
@@ -0,0 +1,38 @@
+#ifndef CONFIGURATION_STORAGE_H
+#define CONFIGURATION_STORAGE_H
+
+#include <opencv2\core\core.hpp>
+#include <QVariant>
+
+class ConfigurationStorage
+{
+public:
+
+	static ConfigurationStorage& instance();
+	/** \brief Write the \a value into the config at \a configPath
+	*  The file will be rewritten and not realeased!
+	*  \param configPath a path to the config file
+	*  \param node node which will be created in the config file
+	*  \param value value to be written
+	*/
+	bool write(const std::string& configPath, std::string node, QVariant value);
+	
+	/** \brief Read the \a value from \a node in the config at \a configPath
+	*  The variable \a value will be read
+	*  \param configPath a path to the config file
+	*  \param node node which will be created in the config file
+	*  \param type value type from QMetaType
+	*  \param value value readed at \a node 
+	*/
+	bool read(const std::string& configPath,  const std::string& node, int type, QVariant& value);
+
+	void realease();
+
+private:
+	ConfigurationStorage();
+	cv::FileStorage storage_;
+
+	
+};
+#endif // !1
+

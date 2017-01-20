cc3b893cc2d363c9dd9d9f07a0c981d6f4e51dbc
danielkos <daniel.kosinski1991@gmail.com>
2017-01-20 18:47:39 -0500

    Preparation for camera interface in UpldFrame started
    Changes might be usefull

--- Sichtprüfung/UpldFrame.cpp
+++ Sichtprüfung/UpldFrame.cpp
@@ -17,7 +17,6 @@ UpldFrame::~UpldFrame()
 {
 	//delete videoCapture_;
 }
-
 /*
 bool UpldFrame::openCamera()
 {
@@ -38,51 +37,55 @@ cv::Mat UpldFrame::fromFile(std::string filename)		//open an filename image
 	return img;
 }
 /*
+cv::Mat UpldFrame::currentImg()
+{
+	cv::Mat ret;
+	imgLock_.lock();
+	currentImg_.copyTo(ret);
+	imgLock_.unlock();
+
+	return ret;
+}
+
 cv::Mat* UpldFrame::fromCamera(bool returnImg)
 {
-	cv::Mat frame;
-	exit_ = false;
-
-
-	cv::putText(frame, "Camera not opened!", cv::Point(50, 50), cv::FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(255));
-
 	// Set camera parameters. Various parameters can be set, see:
 	// http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html
 	if  (videoCapture_ != 0)
 	{ 
 		videoCapture_->set(CV_CAP_PROP_FRAME_WIDTH, CAMERA_WIDHT);
 		videoCapture_->set(CV_CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT);
-		//videoCapture_->set(CV_CAP_PROP_BRIGHTNESS, 2);
-		//videoCapture_->set(CV_CAP_PROP_CONTRAST, 2);
 		videoCapture_->set(CV_CAP_PROP_FPS, CAMERA_FPS);
 
 	}
+
+	cv::putText(currentImg_, "Camera not opened!", cv::Point(50, 50), cv::FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(255));
 
 	while (!exit_)
 	{
 		if (videoCapture_->isOpened())
 		{
-			frame.release();
+			imgLock_.lock();
+			currentImg_.release();
 			// get a new frame from camera
-			videoCapture_->read(frame);
+			videoCapture_->read(currentImg_);
+
+			imgLock_.unlock();
 
 			if (returnImg)
 			{
 				exit_ = true;
 			}
 
-			Q_EMIT newCameraImage(&frame);
+			Q_EMIT newCameraImage(&currentImg_);
 		}
 		else if (returnImg)
 		{
 			exit_ = true;
 		}
-
-		// Delay between two camera frames, to have some time for calculations on an image
-		std::this_thread::sleep_for(std::chrono::milliseconds(CAMERA_RECORD_DELAY));
 	}
 
 	videoCapture_->release();
 
-	return &frame;
+	return &currentImg_;
 }*/
--- Sichtprüfung/UpldFrame.h
+++ Sichtprüfung/UpldFrame.h
@@ -3,6 +3,9 @@
 
 #include <functional>
 #include <opencv2\opencv.hpp>
+#include <mutex>
+
+#include <QObject>
 
 namespace cv
 {
@@ -10,18 +13,24 @@ namespace cv
 	class VideoCapture;
 }
 
-class UpldFrame
+class UpldFrame //: public QObject
 {
+	//Q_OBJECT
 	
 public:
 	UpldFrame();
 	virtual ~UpldFrame();
 
 	static cv::Mat fromFile(std::string file_name);
-	//cv::Mat* fromCamera(bool returnImg = false);
-	//void terminateCameraStream();
-	//bool openCamera();
+	/*cv::Mat* fromCamera(bool returnImg = false);
+	void terminateCameraStream();
+	bool openCamera();
 
+	cv::Mat currentImg();
+
+Q_SIGNALS:
+	void newCameraImage(cv::Mat* img);
+	
 private:
 
 	// Time delay (in milliseconds) between the recording of two camera frames.
@@ -29,12 +38,14 @@ private:
 	//const long CAMERA_RECORD_DELAY = 90;
 
 	// Camera parameters
-	//const double CAMERA_WIDHT = 2048;
-	//const double CAMERA_HEIGHT = 1536;
-	//const double CAMERA_FPS = 11.2;
+	const double CAMERA_WIDHT = 2048;
+	const double CAMERA_HEIGHT = 1536;
+	const double CAMERA_FPS = 11.2;
 
-	//cv::VideoCapture* videoCapture_;
-	//bool exit_;
-	std::string cameraName_;
+	cv::VideoCapture* videoCapture_;
+	bool exit_;
+	cv::Mat currentImg_;
+	std::mutex imgLock_;
+	*/
 };
 #endif
